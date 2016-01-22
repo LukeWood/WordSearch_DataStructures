b66e8c38f0d1b73bdd1f85ca58d3129227efd5fb
lukewoodSMU <lswood@smu.edu>
2016-01-21 23:07:41 -0600

    added in the correct formatting for the input file and created a way to print to the output file.

--- main.cpp
+++ main.cpp
@@ -1,77 +1,106 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdlib>
 #include <vector>
-#include <pair>
-using std::ifstream;
-using std::ofstream;
-using std::cout;
-using std::vector;
-using std::string;
-using std::pair;
+
+using namespace std;
+//Basic function to determine if the char getting input is a digit
+bool isNum(char c) 
+{
+   return (c >='0' && c <='9');
+}
 
 //Creating a Point type to store in my vector of answers
-typedef std::pair<double, double> Point;
+typedef std::pair<int, int> Point;
+
+//Creating a struct for each word we find containing the point at and word name.
+struct foundword
+{
+	string word;
+	Point location;
+	string dir;
+};
+//Creating a pseudo constructor to make it easier to get a foundword
+foundword create_found_word(string word, int x,int y, string dir)
+{
+	foundword toreturn;
+	Point temp(x,y);
+	toreturn.word = word;
+	toreturn.location = temp; 
+	return toreturn;
+}
+
+//Function to write a foundword to a file
+void write_found_word(foundword wfound, ofstream file)
+{
+cout<<wfound.word<<"|"<<wfound.location.first<<"|"<<wfound.location.second<<"|"<<wfound.dir<<"\n";
+}
 
 int main(int argc, char*argv[])
 {
 //Check to ensure that there are two strings passed to the program, the input file then output file.
-if(argc!=4)
+if(argc!=3)
 {
-cout<<"need 3 command prompt arguments, wordbank file, wordsearch file and output file."<<"\n";
+cout<<"need 2 command prompt arguments, wordsearch file and output file."<<"\n";
 return 0;
 }
 
 //Continue on in program, declaring the variables I will need.
 ifstream in;
-vector<string> inputContent;
-vector<string> wordBank;
-vector<Point> locations;
+vector<string> wordSearch;
+string* wordBank;
+vector<foundword> found;
+int wbanklen = 0;
+
 //Trying to open the ifstream to the file input and read each line into the vector of sting, keep track of size before \n.
 in.open(argv[1]);
 if(in.is_open())
 {
 	cout<<"Reading in the word search.\n";
-	string temp = "";
-	char x;
-	while (in>>x)
+	string x;
+	bool done = false;
+	while (in>>x && !done)
 	{
-		temp+= x;
-		if(in.peek()=='\n')
+		if(isNum(x[0]))
 		{
-		inputContent.push_back(temp);
-		temp = "";
+		done = true;
+		wbanklen = atoi(x.c_str());
+		break;
+		}
+		else
+		{
+		wordSearch.push_back(x);
 		}
 	}
-}
 
 //Just showing that I have successfully read in the word search and closing the ifstream.
-cout<<"Full Word Search:\n";
-for(int i = 0; i < inputContent.size(); i++)
-{
-	cout<<inputContent[i]<<"\n";
-}
-in.close();
-cout<<"\n";
-//Reading in the word bank.
-in.open(argv[2]);
-if(in.is_open())
-{
-	cout<<"Reading in the word bank.\n";
-	string word;
-	while(in>>word)
+	cout<<"Full Word Search:\n";
+	for(int i = 0; i < wordSearch.size(); i++)
 	{
-	wordBank.push_back(word);
+		cout<<wordSearch[i]<<"\n";
 	}
+	cout<<"\n"<<"Reading in word bank.\n";
+//Reading in the word bank.
+	wordBank = new string[wbanklen];
+	for(int i = 0; i < wbanklen;i++)
+	{
+		string word;
+		in>>word;
+		wordBank[i] = word;
+	}	
+	in.close();
+	cout<<"Full word bank:\n";
+	for(int i = 0; i < wbanklen;i++)
+	{
+		cout<<wordBank[i]<<"\n";
+	}
+	cout<<"Beginning to solve word search.\n";
 }
-in.close();
-
-cout<<"Full word bank:\n";
-for(int i = 0; i < wordBank.size();i++)
+else
 {
-cout<<wordBank[i]<<"\n";
+	cout<<"File was not opened.\n";
 }
-
 //End Program
 return 0;
 }
